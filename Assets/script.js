f86196d6139fd2571c13aff810ba4d4d439a7064
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-01-19 16:09:28 -0600

    added luxon dateTime to add date to top of page

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
+    
+    // print current date to top of page 
+    var localTime = luxon.DateTime.local();
+    var formatTime = localTime.toLocaleString(luxon.DateTime.DATE_MED_WITH_WEEKDAY)
+    $("#currentDay").text(formatTime);
+    
+    
+    // on click listener for the save button 
+    // save values of text to local storage
+    // values will be from the textarea
+
+    // function that uses moment.js and updates each color block in textFill to represent current time 
+
+    // load any saved data from local storage to correct div
+
+    // $("#current-day").text([PUT MOMEMENT FUNCTION HERE])
 
 });
