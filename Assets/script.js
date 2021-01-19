2a8920a453c08a3de9c820fde96e5ffe47c4cbcc
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-01-19 17:39:38 -0600

    finished if/else if/else statements that fill textFill with correct colors though the use of this

--- Assets/script.js
+++ Assets/script.js
@@ -4,22 +4,23 @@ $(document).ready(function() {
     var localTime = luxon.DateTime.local();
     var formatTime = localTime.toLocaleString(luxon.DateTime.DATE_MED_WITH_WEEKDAY)
     $("#currentDay").text(formatTime);
-    
-    
- 
 
     // function that uses luxon and updates each color block in textFill to represent current time 
-
     
     var hour = localTime.hour // create a variable that pulls the current hour using luxon
     
-    if (hour === ) { // if time block is present hour
-        $(".textFill").addClass("present"); // make textarea red (use .present css sytling)
-    } else if(hour < ) {  // else if time block is greater than present hour
-        $(".textFill").addClass("future");  // make textarea green (use .future css styling)
-    } else {  // else 
-        $(".textFill").addClass("past"); // make textarea gray (use .past css styling)
-    }
+    $(".textFill").each(function(){
+        var elementTime = parseInt($(this).attr("time-hour"))
+        if (hour === elementTime) { // if time block is present hour
+            $(this).addClass("present"); // make textarea red (use .present css sytling)
+        } else if(hour > elementTime) {  
+            $(this).addClass("past");  
+        } else {  
+            $(this).addClass("future");
+        }
+    })
+
+    
 
     // load any saved data from local storage to correct div
     // on click listener for the save button 
